a111349bc56a68c791fe1c9167260275f7cb9690
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-10 11:02:16 +0900

    added validators for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  #validators
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: 0..5
 end
