7ae156d4882645d8cf5b333a2e92d369669c6ea3
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-10 10:54:14 +0900

    created validators for restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  #validators
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
